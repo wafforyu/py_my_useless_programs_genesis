e93409521d474e98546958a7372a885fec7974da
wafforyu <72655919+wafforyu@users.noreply.github.com>
2021-05-06 18:50:11 +0800

    change function names for moves and defaults

--- pokemon_idlerpg.py
+++ pokemon_idlerpg.py
@@ -5,7 +5,7 @@ import pyautogui
 #ctrl+c on terminal to stop the program.
 def idle_rpg():
     def adventure():
-        pyautogui.typewrite("$adventure 1") #just change adventure
+        pyautogui.typewrite("$adventure 2") #just change adventure
         pyautogui.press("enter")
 
     def status():
@@ -24,29 +24,31 @@ def pokemon():
     def route():
             pyautogui.typewrite(".route 6") #change route number
             pyautogui.press('enter')
-            time.sleep(6) #delay
+            time.sleep(5) #delay
+            
+    def _1():
+        pyautogui.typrewrite("1")
+        pyautogui.press('enter')
+        
+    def _2():
+        pyautogui.typewrite("2")
+        pyautogui.press('enter')
 
-    def leech_seed():
+    def _3():
         pyautogui.typewrite("3")
         pyautogui.press('enter')
-        time.sleep(6)
 
-    def tackle():
-        pyautogui.typewrite("1")
-        pyautogui.press('enter')
-        time.sleep(6)
-
-    def vine_whip():
+    def _4():
         pyautogui.typewrite("4")
         pyautogui.press('enter')
-        time.sleep(6)
 
     time.sleep(10)
     x = 1 
     while (x):
         route() #route 
         for i in range(0,2): #number of times you attack
-            vine_whip()
+            _1()
+            time.sleep(4) #delay
 
 
 if __name__ == '__main__':
