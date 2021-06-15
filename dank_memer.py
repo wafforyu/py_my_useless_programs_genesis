2a2c3d236dc71e2310c30b3af0a09cd4a056d8d1
wafforyu <wafforyu@gmail.com>
2021-06-15 15:44:04 +0800

    add human action emulation

--- dank_memer.py
+++ dank_memer.py
@@ -1,9 +1,28 @@
 import time
 from threading import Thread
 from pyautogui import typewrite, sleep, press
-from random import choice
+from random import choice, randint
 
 #ctrl+c on terminal to stop the program.
+def bal():
+    typewrite('pls bal')
+    press('enter')
+
+def shop_inv():
+    number = str(randint(1,6))
+    sleep(3)
+    typewrite('pls shop '+ number)
+    press('enter')
+
+    number = str(randint(1,6))
+    sleep(3)
+    typewrite('pls inv '+ number)
+    press('enter')
+
+def dep():
+    sleep(2)
+    typewrite('pls dep all')
+    press('enter')
 
 def postmemes():
     arr = ['f','r','i','c','k']
@@ -17,31 +36,31 @@ def postmemes():
 
 def trivia():
     arr = ['a','b','c','d']
-    sleep(3)
+    sleep(2)
     typewrite('pls trivia')
     press('enter')
     
-    sleep(1)
+    sleep(3)
     typewrite(choice(arr))
     press('enter')
     
 def beg():
-    sleep(3)
+    sleep(2)
     typewrite('pls beg')
     press('enter')
 
 def dig():
-    sleep(3)
+    sleep(2)
     typewrite('pls dig')
     press('enter')
 
 def fish():
-    sleep(3)
+    sleep(2)
     typewrite('pls fish')
     press('enter')
 
 def hunt():
-    sleep(3)
+    sleep(2)
     typewrite('pls hunt')
     press('enter')
 
@@ -53,8 +72,11 @@ def main():
         dig()
         fish()
         hunt()
-        sleep(40)
         postmemes()
+        dep()
+        shop_inv()
+        sleep(15)
+        bal()
         
 if __name__ == '__main__':
     print("running")
