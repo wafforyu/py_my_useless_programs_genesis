067365f86e22ae5a2a1c2f17658283f22ba01faf
wafforyu <wafforyu@gmail.com>
2021-06-16 21:28:00 +0800

    improve random actions

--- dankaf.py
+++ dankaf.py
@@ -5,24 +5,43 @@ from random import choice, randint, shuffle
 
 #ctrl+c on terminal to stop the program.
 
-def memey():
-    arr = ['4chan', 'antiantjoke', 'blacktwitter','chucknorris','comics','discordmeme','joke','meirl','prequel','pun','sequel']
-    typewrite('pls ' + choice(arr))
+def guess():
+    typewrite('pls guess')
     sleep(2)
     press('enter')
 
+    for i in range(0,4):
+        typewrite(str(randint(1,20)))
+        sleep(1)
+        press('enter')
+
+def memey():
+    random_sleep = randint(7,12)
+    arr = ['4chan', 'blacktwitter','chucknorris','comics','discordmeme','joke','meirl','prequel','pun','sequel']
+    typewrite('pls ' + choice(arr))
+    sleep(2)
+    press('enter')
+    sleep(random_sleep)
+
 def bal():
+    sleep(2)
     typewrite('pls bal')
     press('enter')
 
-def shop_inv():
+    r = randint(0,1)
+    if r:
+        dep()
+
+def shop():
     random_sleep = randint(2,6)
     number = str(randint(2,5))
     typewrite('pls shop '+ number)
     sleep(random_sleep)
     press('enter')
 
-    number = str(randint(2,6))
+def inv():
+    random_sleep = randint(2,6)
+    number = str(randint(2,4))
     typewrite('pls inv '+ number)
     sleep(random_sleep)
     press('enter')
@@ -40,7 +59,7 @@ def postmemes():
     sleep(random_sleep)
     press('enter')
 
-    random_sleep = randint(2,4)
+    random_sleep = randint(1,3)
     typewrite(choice(arr))
     sleep(random_sleep)
     press('enter')
@@ -82,25 +101,57 @@ def hunt():
     press('enter')
 
 def level():
-    random_sleep = randint(2,6)
-    typewrite('pls level')
-    sleep(random_sleep)
+
+    r = randint(0,1)
+    if r:
+        random_sleep = randint(2,6)
+        typewrite('pls level')
+        sleep(random_sleep)
+        press('enter')
+    
+def active():
+
+    r = randint(0,1)
+    if r:
+        random_sleep = randint(2,6)
+        typewrite('pls level --active')
+        sleep(random_sleep)
+        press('enter')
+
+def random_word():
+    random_word = ["bark","mighty","yourself","finally","rays","what"
+"climate","any","cabin","sure","laugh","paid"
+"city","excitement","different","buy","article","way"
+"dinner","soil","hearing","stop","hearing","broad"
+"population","hand","off","exclaimed","giving","young"
+"riding","buried","poet","including","early","do"
+"surface","graph","skin","use","dot","wheel"]
+    typewrite(choice(random_word))
     press('enter')
 
-arr_functions = [memey, bal, shop_inv, postmemes,trivia, beg, dig, fish, hunt, memey]
+def chance():
+    flip = randint(0,19)
+    if flip == 1:
+        print("5% just happened")
+        return True
+    return False
+
+arr_functions = [guess, random_word, postmemes , bal, trivia, level, beg, dig, fish, hunt]
+
 
 def main():
-    random_sleep = randint(18,24)
     x = 1
     while x:
+        random_sleep = randint(20,35)
         shuffle(arr_functions)
         for f in arr_functions:
             f()
         sleep(random_sleep)
-        dep()
+        active()
+        if chance():
+            sleep(180)
         
 if __name__ == '__main__':
     print("running")
-    sleep(5)
     main()
 
