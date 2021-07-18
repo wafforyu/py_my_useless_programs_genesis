3e02194fabfeaef76a510d1b69bb6846cf10aca7
wafforyu <wafforyu@gmail.com>
2021-07-18 13:41:51 +0800

    add avg of the three groups

--- theory_of_randomness.py
+++ theory_of_randomness.py
@@ -1,8 +1,7 @@
 #just had a thought. if the odds of an event is 1/30 and you do it 8 times and in those 8 times you randomly pick 1
 #then let's test 1/30*8*1/8 is still 1/30 by running like thousands of simulations
 
-import random
-
+import random, statistics
 
 def for_a(n):
     a = 0
@@ -33,21 +32,34 @@ def for_c(n):
     c = 0
 
     for x in range(n):
+        rng = random.randint(0,29)
+        r = []
         for y in range(8,0,-1):
-            rng = random.randint(0,29)
-            r = []
             for i in range(0,y):
                 r.append(random.randint(0,29))
-            if r[random.randint(0,len(r)-1)] == rng:
-                c += 1
+        if r[random.randint(0,len(r)-1)] == rng:
+            c += 1
     
     return c
 
 def main():
+    a = []
+    b = []
+    c = []
 
-    print("a", for_a(300000))
-    print("b", for_b(300000))
-    print("c", for_c(300000))
+    for i in range(10000):
+        a.append(for_a(30))        
+        b.append(for_b(30))        
+        c.append(for_c(30))        
+
+    avg_a = statistics.mean(a)
+    avg_b = statistics.mean(b)
+    avg_c = statistics.mean(c)
+
+    print("a" ,avg_a)
+    print("b" ,avg_b)
+    print("c" ,avg_c)
+
     #a = 0
     #b = 0
 
